c7359cc108f54ad6443567745db2429a87e68714
waribu <gwaribu@gmail.com>
2022-08-10 19:03:33 +0300

    bot addition

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,10 +11,11 @@ const io = socketio(server);
 // static folder setting
 app.use(express.static(path.join(__dirname, "public")));
 
+const botName = "JabaChat Bot";
 // client connection listener
 io.on("connection", (socket) => {
   // Welcome current User
-  socket.emit("message", "Welcome to Jaba Chat!");
+  socket.emit("message", formatMessage, (botName, "Welcome to Jaba Chat!"));
   // Brodcast when a user connects
   socket.broadcast.emit("message", "A user has joined the chat!");
 
@@ -21,10 +23,10 @@ io.on("connection", (socket) => {
     io.emit("message", "User has left the chat!");
   });
 
-  // listen for chatMessage 
-  socket.on('chatMessage', msg => {
-    io.emit('message', msg)
-  })
+  // listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", msg);
+  });
 });
 
 const PORT = 3030 || process.env.PORT;
